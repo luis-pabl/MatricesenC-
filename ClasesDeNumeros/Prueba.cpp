627c6aa4cb73b844983d7e58eb5b301b6e6436d2
lujeolhe <lujeolhe@live.com.mx>
2021-02-13 12:35:32 -0600

    Se agregan algunas exepciones en la clase Naturales

--- ClasesDeNumeros/Prueba.cpp
+++ ClasesDeNumeros/Prueba.cpp
@@ -3,10 +3,32 @@
 using namespace std;
 using namespace NUMEROS;
 int main(){
-  Naturales a,b,c;
-  Naturales d(1),e(1);
+  Naturales a(5),b(6),c;
+  Naturales d(1),e(1),f(1);
+
+  try{
+    std::cout << "Ingresa un numero Natural" << '\n';
+    cin>>a.n;
+    //a=numeroDeEntrada;
+  }
+  catch (const char *e){
+    cout << e << '\n';
+    //a=numeroDeEntrada*-1;
+    cout << e << "Cambie tu numero a"<< a <<'\n';
+  }
+
+  e=(2*a-4)/2;
+  cout<<"EL valor de e es: "<<e<<" EL valor de e es: "<<f<<endl;
   cout<<"Valor de la suma de a:"<<a<<" + b: "<<b<<" = c:"<<a+b<<endl;
-  cout<<"Valor de la suma de a:"<<a<<" - b: "<<b<<" = c:"<<a-b<<endl;
+  try{
+    cout<<"Valor de la resta de a:"<<a<<" - b: "<<b<<" = c:"<<a-b<<endl;
+  }
+  catch(const char *s ) {
+  cout << "\nError numero: " << s <<endl;
+  }
+
   cout<<"Valor de la suma de a:"<<a<<" * b: "<<b<<" = c:"<<a*b<<endl;
   cout<<"Valor de la suma de a:"<<d<<" / b: "<<e<<" = c:"<<d/e<<endl;
+
+  cout<<"Valor de la potencia a la 3:"<< b.pow(3) <<endl;
 }
